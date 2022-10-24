c23eef3eb873eb1c4f981dbacc126fb0bd3d5ac6
Ronaldo Lantigua Martines <20198855@itla.edu.do>
2022-10-24 19:42:06 -0400

    completed step by step of home page

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -26,6 +26,17 @@ const Home =()=>{
                         <p>A continuacion inicia sesion</p>
                         <Link className='btn btn-primary my-3' to={'/login'}>Login</Link>
                     </div>
+                    <div className='step'>
+                        <h3 className='p-1'>Paso 3</h3>
+                        <img src="https://cdn-icons-png.flaticon.com/512/2343/2343731.png" alt="registro.png" />
+                        <p>Luego llene el formulario de solicitud con los datos correspondientes</p>
+                        <Link className='btn btn-primary my-3' to={'/formulario'}>Formulario</Link>
+                    </div>
+                    <div className='step'>
+                        <h3 className='p-1'>Paso 4</h3>
+                        <img src="https://cdn-icons-png.flaticon.com/512/1584/1584892.png" alt="registro.png" />
+                        <p>Ahora debe esperar que su solicitud sea aprobada</p>
+                    </div>
                 </div>
             </div>
         </div>
